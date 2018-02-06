414530c50112681f70b7206fc160fd44dd39642f
Clay Knight <clayknight@rocketmail.com>
2018-02-06 11:12:23 -0800

    Testing login route1

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -77,20 +77,20 @@ module.exports = function (app) {
             })
             .catch(error => res.send(error.message));
     });
+
+
+    app.post('/login', (req, res) => {
+        let baseUrl = app.get('url').replace(/\/$/, '');
+        console.log(baseUrl);
+        const { email, password } = req.body;
+        console.log("email login", email)
+        console.log("password login", password)
+        //Create a new user
+
+        //Once user has signed up successfully, log them in
+        axios.post(baseUrl + '/api/users/login', { email, password })
+            .then(r => console.log("res login",r))
+            .catch(e => res.send(e.message))
+    });
+
 }
-
-app.get('/login', (req, res) => { res.send('YourE at login')
-    // let baseUrl = app.get('url').replace(/\/$/, '');
-    // console.log(baseUrl);
-    // const { email, password } = req.body;
-    // console.log("email login", email)
-    // console.log("password login", password)
-    // //Create a new user
-
-    // //Once user has signed up successfully, log them in
-    // axios.post(baseUrl + '/api/users/login', { email, password })
-    //     .then(r => console.log("res login",r))
-    //     .catch(e => res.send(e.message))
-});
-
-
