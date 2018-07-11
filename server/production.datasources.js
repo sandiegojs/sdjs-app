1dce497a4242b0c49e0e7424db610b1ffc736b86
Brian Austin <baustin27@gmail.com>
2018-07-10 17:21:28 -0700

    Ba sdjsapp 18 (#4)
    * on login page changed create new user clickalbe text to button with Create Account as text
    
    * on login page changed create new user clickalbe text to button with Create Account as text
    
    * updated datasources.development.json, production.datasources.js, and datasources.json file to properly get local server running

---
+++ server/production.datasources.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "MongoDB": {
+  "host": process.env.MONGO_HOST,
+  "port": process.env.MONGODB_PORT,
+  "url": process.env.MONGO_URI,
+  "database": process.env.MONGODB_DATABASE,
+  "password": process.env.MONGODB_PW,
+  "name": "MongoDB",
+  "user": process.env.MONGODB_USER,
+  "connector": "loopback-connector-mongodb",
+  "file":"db.json"
+  }
+}
