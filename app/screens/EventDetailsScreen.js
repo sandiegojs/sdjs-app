c5926803a9dd21e4bfd6457716ef3b8766256b6e
ccficorilli <ccficorilli@gmail.com>
2018-07-11 14:06:22 -0700

    added back-button functionality to event details page instead of app exit choice

--- app/screens/EventDetailsScreen.js
+++ app/screens/EventDetailsScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, BackHandler } from 'react-native';
 import EventDetailsContainer from '../containers/EventDetailsContainer/EventDetailsContainer';
 
 export default class EventDetailsScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+}
+
+componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
+}
+
+componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+}
+
+handleBackButtonClick() {
+    this.props.navigation.goBack(null);
+    return true;
+}
   static navigationOptions = {
     title: 'Event Details',
   };
