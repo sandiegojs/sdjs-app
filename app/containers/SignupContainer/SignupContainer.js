8d85846d1fc77e23b845542032f2cdeb9d58a682
Brian <baustin27@gmail.com>
2018-07-12 17:15:43 -0700

    deleting whitespace and console logs as requested by MR

--- app/containers/SignupContainer/SignupContainer.js
+++ app/containers/SignupContainer/SignupContainer.js
@@ -30,7 +30,7 @@ class SignupContainer extends React.Component {
     }
 
     handleLastNameInput(text) {
-        const { dispatch } = this.props;    
+        const { dispatch } = this.props;
         dispatch(lastNameEntry(text));
     }
 
