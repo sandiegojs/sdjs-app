c4b632800df6d56644d2420f57757789c9ad1367
Michael Roberts Jr <michael.e.roberts.jr@gmail.com>
2018-07-16 14:57:02 -0700

    adds config changes for mlabs and now deployment (#18)

---
+++ server/datasources.development.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'MongoDB': {
+    'url': 'mongodb://localhost:27017/sdjs-app',
+    'name': 'MongoDB',
+    'connector': 'loopback-connector-mongodb',
+    'file': 'db.json',
+  },
+};
---
+++ server/datasources.production.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'MongoDB': {
+    'url': process.env.MONGO_URI,
+    'name': 'MongoDB',
+    'connector': 'loopback-connector-mongodb',
+  },
+};
