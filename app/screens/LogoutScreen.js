e2a568ea9f165118f2a2620b99a052fc84a4662d
KurisuLim <kazuki1002003@yahoo.com>
2018-08-10 03:20:18 -0700

    adds a new file to be used in the logout feature

---
+++ app/screens/LogoutScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import LogoutContainer from '../containers/LogoutContainer/LogoutContainer';
+
+export default class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Log Out',
+    headerLeft: null,
+  };
+  
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <LogoutContainer navigation={this.props.navigation} />
+    
+    );
+  }
+};
