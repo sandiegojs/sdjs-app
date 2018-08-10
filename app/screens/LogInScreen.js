039263763d44da00d480585934f842fe9a1cdad1
KurisuLim <kazuki1002003@yahoo.com>
2018-08-10 03:19:30 -0700

    adds headerleft to remove the login button at the header

--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -6,7 +6,9 @@ import LoginContainer from '../containers/LoginContainer/LoginContainer';
 export default class LoginScreen extends React.Component {
   static navigationOptions = {
     title: 'Log In',
+    headerLeft: null,
   };
+  
   render() {
     const { navigate } = this.props.navigation;
     return (
