a4379fc63142abc72e53ce8c7cc0dcfd381fd943
KurisuLim <kazuki1002003@yahoo.com>
2018-08-10 03:16:15 -0700

    Removes the changes I made earlier

--- app/screens/SDJSScreen.js
+++ app/screens/SDJSScreen.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import SDJSContainer from '../containers/SDJSContainer/SDJSContainer';
-import LogOutContainer from '../containers/LogoutContainer/LogoutContainer';
 
 export default class SDJSScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: '.Org',
     headerTitle:'Visit Us',
     headerLeft: null,
-    headerRight: <LogOutContainer />,
     
   };
   render() {
