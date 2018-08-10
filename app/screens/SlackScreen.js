34723139afd909f56a15cd6d7e410809eb0f00c6
KurisuLim <kazuki1002003@yahoo.com>
2018-08-10 03:11:06 -0700

    Removes changes I made earlier

--- app/screens/SlackScreen.js
+++ app/screens/SlackScreen.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Platform } from 'react-native';
 import SlackContainer from '../containers/SlackContainer/SlackContainer';
-import LogOutContainer from '../containers/LogoutContainer/LogoutContainer';
 
 export default class SlackScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: 'Slack',
     headerTitle: 'Join us',
     headerLeft: null,
-    headerRight: <LogOutContainer />,
-    
   };
 
   render() {
