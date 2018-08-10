9e44130c2dae79dee5d547f0e8490e657795259c
KurisuLim <kazuki1002003@yahoo.com>
2018-08-10 03:18:00 -0700

    Removes white spaces

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import ProfileContainer from '../containers/ProfileContainer/ProfileContainer'
-import LogOutContainer from '../containers/LogoutContainer/LogoutContainer';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
     title: 'Profile',
     tabBarLabel: 'Profile',
     headerLeft: null,
-    headerRight: <LogOutContainer />,
   };
+  
   render() {
     const { navigate } = this.props.navigation;
     return (
