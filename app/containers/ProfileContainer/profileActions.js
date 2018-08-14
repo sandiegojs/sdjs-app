c74bff372f3187da22904b42af722a1cdf9c9fd8
Phillip Truong <pltruong2507@gmail.com>
2018-08-13 18:01:07 -0700

    Enable ACL, clarify store tree (#49)
    ### In This Commit
    
    - Enables ACL and requires server authentication
    - all api requests now include user login auth token in header
    - implements reduce-reducers package to combine signup and login store trees for user credentials to be used by other containers
    - removes redundant user info and credential fields in store
    - updates references to signup and login store trees in various files, to consolidated userData tree
    - removes several unused imports
    - reformats user first and last name model fields from underscore_case to camelCase

--- app/containers/ProfileContainer/profileActions.js
+++ app/containers/ProfileContainer/profileActions.js
@@ -1,60 +1,68 @@
 import axios from 'axios';
 
-export function profileUpdate(newProfileData, id) {
-    return {
-        type: 'PROFILE_UPDATE',
-        payload: axios
-                    .patch('https://sdjs-app.now.sh/api/users/' + id, newProfileData)
-                    .then(r => r.data)
-
-    }
+export function profileInit(id, token) {
+  return {
+    type: 'PROFILE_INIT',
+    payload: axios
+      .get('https://sdjs-app.now.sh/api/users/' + id, {headers: {Authorization: token}})
+      .then(r => r.data)
+  }
 }
 
-export function firstNameUpdate(first_name) {
-    return {
-        type: 'FIRST_NAME_UPDATE',
-        payload: first_name
-    }
+export function profileUpdate(newProfileData, id, token) {
+  return {
+    type: 'PROFILE_UPDATE',
+    payload: axios
+      .patch('https://sdjs-app.now.sh/api/users/' + id, newProfileData, {headers: {Authorization: token}})
+      .then(r => r.data)
+  }
 }
 
-export function lastNameUpdate(last_name) {
-    return {
-        type: 'LAST_NAME_UPDATE',
-        payload: last_name
-    }
+export function firstNameUpdate(firstName) {
+  return {
+    type: 'FIRST_NAME_UPDATE',
+    payload: firstName
+  }
+}
+
+export function lastNameUpdate(lastName) {
+  return {
+    type: 'LAST_NAME_UPDATE',
+    payload: lastName
+  }
 }
 
 export function emailUpdate(email) {
-    return {
-        type: 'EMAIL_UPDATE',
-        payload: email
-    }
+  return {
+    type: 'EMAIL_UPDATE',
+    payload: email
+  }
 }
 
 export function bioUpdate(bio) {
-    return {
-        type: 'BIO_UPDATE',
-        payload: bio
-    }
+  return {
+    type: 'BIO_UPDATE',
+    payload: bio
+  }
 }
 
 export function companyUpdate(company) {
-    return {
-        type: 'COMPANY_UPDATE',
-        payload: company
-    }
+  return {
+    type: 'COMPANY_UPDATE',
+    payload: company
+  }
 }
 
 export function urlUpdate(url) {
-    return {
-        type: 'URL_UPDATE',
-        payload: url
-    }
+  return {
+    type: 'URL_UPDATE',
+    payload: url
+  }
 }
 
 export function locationUpdate(location) {
-    return {
-        type: 'LOCATION_UPDATE',
-        payload: location
-    }
+  return {
+    type: 'LOCATION_UPDATE',
+    payload: location
+  }
 }
