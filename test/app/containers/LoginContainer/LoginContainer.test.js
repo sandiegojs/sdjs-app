190acf7964652556f2f10c131b6f7aa8addae337
RenzoPozo <renzopozo@yahoo.com>
2018-08-21 10:58:31 -0700

    adding test unit files

---
+++ test/app/containers/LoginContainer/LoginContainer.test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+const LoginContainer = require ('../../../../app/containers/LoginContainer/LoginContainer.js')
+
+test('it sends information of the user', () => {
+    function LoginContainer(data) {
+    expect(data).toBe('')}
+})
