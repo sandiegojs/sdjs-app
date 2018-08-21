07939b516db6f9c3d230aaf0ed0da3957955bcb4
RenzoPozo <renzopozo@yahoo.com>
2018-08-21 15:08:24 -0700

    passing new files to test folder

---
+++ test/app/containers/LoginContainer/LoginActions.test.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill'
+
+const expect = require('chai').expect;
+const LoginActions = require ('../../../../app/containers/LoginContainer/LoginActions')
+
+
+describe('Login Actions', () => {
+   console.log(LoginActions);
+    it('should take users email address', () => {
+        expect(LoginActions.updateEmailInput).to.exist;
+    });
+    it('should take users password', () => {
+        expect(LoginActions.updatePasswordInput).to.exist;
+    });
+    it('should take users password', () => {
+        expect(LoginActions.submitLogin).to.exist;
+    });
+})
