dc2d157f5b7b1755de0b3c99e5b4107ae3502e96
Christopher Lim <kazuki1002003@yahoo.com>
2018-08-23 15:55:05 -0700

    Fix the textInput for Android Users (#87)
    * accepts rebase
    
    * Removes FormInput on import
    
    * removes comments and did eslint fix
    
    * fixed the json missing files
    
    * Re adds the missing package to package.json
    
    * not used package

--- test/app/containers/EventsContainer/eventsActions.test.js
+++ test/app/containers/EventsContainer/eventsActions.test.js
@@ -1,14 +1,14 @@
 const expect = require('chai').expect;
-const EventsActions = require('../../../../app/containers/EventsContainer/eventsActions')
+const EventsActions = require('../../../../app/containers/EventsContainer/eventsActions');
 
 describe('Events Actions', () => {
-    it(' checks for existance location error', () => {
-        expect(EventsActions.setLocationError).to.exist;
-    });
-    it('checks for existance of checked-true', () => {
-        expect(EventsActions.checkedInTrue).to.exist;
-    });
-    it('checks for existance of checked-false', () => {
-        expect(EventsActions.checkedInFalse).to.exist;
-    });
-})
+  it(' checks for existance location error', () => {
+    expect(EventsActions.setLocationError).to.exist;
+  });
+  it('checks for existance of checked-true', () => {
+    expect(EventsActions.checkedInTrue).to.exist;
+  });
+  it('checks for existance of checked-false', () => {
+    expect(EventsActions.checkedInFalse).to.exist;
+  });
+});
--- test/app/containers/SignupContainer/SignupActions.test.js
+++ test/app/containers/SignupContainer/SignupActions.test.js
@@ -1,20 +1,20 @@
 const expect = require('chai').expect;
-const SignupActions = require('../../../../app/containers/SignupContainer/SignupActions')
+const SignupActions = require('../../../../app/containers/SignupContainer/SignupActions');
 
 describe('Signup Actions', () => {
-    it('checks for existance of first name input', () => {
-        expect(SignupActions.updateFirstNameInput).to.exist;
-    });
-    it('checks for existance of last name input', () => {
-        expect(SignupActions.updateLastNameInput).to.exist;
-    });
-    it('checks for existance of emai input', () => {
-        expect(SignupActions.updateEmailInput).to.exist;
-    });
-    it('checks for existance of password input', () => {
-        expect(SignupActions.updatePasswordInput).to.exist;
-    });
-    it('checks for existance of signup', () => {
-        expect(SignupActions.submitSignUp).to.exist;
-    });
-})
+  it('checks for existance of first name input', () => {
+    expect(SignupActions.updateFirstNameInput).to.exist;
+  });
+  it('checks for existance of last name input', () => {
+    expect(SignupActions.updateLastNameInput).to.exist;
+  });
+  it('checks for existance of emai input', () => {
+    expect(SignupActions.updateEmailInput).to.exist;
+  });
+  it('checks for existance of password input', () => {
+    expect(SignupActions.updatePasswordInput).to.exist;
+  });
+  it('checks for existance of signup', () => {
+    expect(SignupActions.submitSignUp).to.exist;
+  });
+});
