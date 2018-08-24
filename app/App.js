765fc0b9dbaf9564a6d6757a2b786c8ff7470176
KurisuLim <kazuki1002003@yahoo.com>
2018-08-24 13:30:11 -0700

    fixes the switch statement

--- app/App.js
+++ app/App.js
@@ -11,7 +11,7 @@ import LoginScreen from './screens/LogInScreen';
 import SDJSScreen from './screens/SDJSScreen';
 import SlackScreen from './screens/SlackScreen';
 import PasswordResetScreen from './screens/PasswordResetScreen';
-import DonateScreen from './screens/DonateScreen';
+// import DonateScreen from './screens/DonateScreen';
 import ThankyouScreen from './screens/ThankyouScreen';
 import LogoutScreen from './screens/LogoutScreen';
 import QuestionnaireScreen from './screens/QuestionnaireScreen';
@@ -64,9 +64,9 @@ class App extends React.Component {
             Events: {
               screen: EventsScreen,
             },
-            Donate: {
-              screen: DonateScreen,
-            },
+            // Donate: {
+            //   screen: DonateScreen,
+            // },
             Profile: {
               screen: ProfileScreen,
             },
@@ -82,42 +82,53 @@ class App extends React.Component {
               tabBarIcon: ({ focused, tintColor }) => {
                 const { routeName } = navigation.state;
                 let iconName, iconSize;
-                if (routeName === 'SanDiegoJS'){
-                  iconName =`home${focused ? '' : '-outline'}`;
-                  iconName ='home';
-                  iconSize=31;
+                switch (routeName) {
+                  case 'SanDiegoJS':
+                    iconName =`home${focused ? '' : '-outline'}`;
+                    iconName ='home';
+                    iconSize=31;
+                    return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
+                  case 'Slack':
+                    iconName =`slack${focused ? '' : '-outline'}`;
+                    iconName ='slack';
+                    iconSize=25;
+                    return  <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
+                  case 'Events':
+                    iconName =`calendar${focused ? '' : '-outline'}`;
+                    iconName ='calendar';
+                    iconSize=23;
+                    return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
+                  // case 'Donate':
+                  //   return (
+                  //     <Image
+                  //       source={require('./assets/images/donate.png')}
+                  //       fadeDuration={0}
+                  //       style={{ width: 30, height: 30 }}
+                  //     />
+                  //   )
+                  //   break;
+                  case 'Profile':
+                    iconName =`user${focused ? '' : '-outline'}`;
+                    iconName ='user';
+                    iconSize=27.5;
+                    return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
+                  case 'Logout':
+                    iconName =`arrow-left${focused ? '' : '-outline'}`;
+                    iconName ='arrow-left';
+                    iconSize=27;
+                    return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
+                  case 'Shopping':
+                    iconName =`shopping-cart${focused ? '' : '-outline'}`;
+                    iconName ='shopping-cart';
+                    iconSize=28;
+                    return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
+                    break;
                 }
-                else if (routeName === 'Slack'){
-                  iconName =`slack${focused ? '' : '-outline'}`;
-                  iconName ='slack';
-                  iconSize=25;
-                }
-                else if (routeName === 'Events'){
-                  iconName =`calendar${focused ? '' : '-outline'}`;
-                  iconName ='calendar';
-                  iconSize=23;
-                }
-                else if (routeName === 'Donate'){
-                  iconName =`gift${focused ? '' : '-outline'}`;
-                  iconName ='gift';
-                  iconSize=30;
-                }
-                else if (routeName === 'Shopping'){
-                  iconName =`shopping-cart${focused ? '' : '-outline'}`;
-                  iconName ='shopping-cart';
-                  iconSize=28;
-                }
-                else if (routeName === 'Profile'){
-                  iconName =`user${focused ? '' : '-outline'}`;
-                  iconName ='user';
-                  iconSize=27.5;
-                }
-                else if (routeName === 'Logout'){
-                  iconName =`arrow-left${focused ? '' : '-outline'}`;
-                  iconName ='arrow-left';
-                  iconSize=27;
-                }
-                return <Ionicons name={iconName}  size={iconSize} color={tintColor}/>
               },
             }),
             tabBarComponent: TabBarBottom,
@@ -129,7 +140,7 @@ class App extends React.Component {
         )
       },
       EventDetails: { screen: EventDetailsScreen },
-      Donate: { screen: DonateScreen },
+      // Donate: { screen: DonateScreen },
       ThankYou: { screen: ThankyouScreen }
     });
     return (
